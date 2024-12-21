43ef295087f63713c04ec32cbcedf04298bc723b
Sunil Shrestha <shresth90@uni.coventry.ac.uk>
2024-12-21 08:04:21 +0545

    using api data on app

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,12 +9,24 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<dynamic> bucketlistData = [];
+
   void getData() async {
     //get data from API
-    Response response = await Dio().get(
-        "https://flutterapitest321-default-rtdb.firebaseio.com/bucketlist.json");
-    print(response.statusCode);
-    print(response.data);
+    try {
+      Response response = await Dio().get(
+          "https://flutterapitest321-default-rtdb.firebaseio.com/bucketlist.json");
+      bucketlistData = response.data;
+      setState(() {});
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+                title: Text(
+                    "Connection to the server error! Please try again later"));
+          });
+    }
   }
 
   @override
@@ -23,7 +35,30 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: Text("Bucket List"),
       ),
-      body: ElevatedButton(onPressed: getData, child: Text("Get Data")),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: getData, child: Text("Get Data")),
+          Expanded(
+            child: ListView.builder(
+                itemCount: bucketlistData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 25,
+                        backgroundImage:
+                            NetworkImage(bucketlistData[index]['image'] ?? ""),
+                      ),
+                      title: Text(bucketlistData[index]['item'] ?? ""),
+                      trailing:
+                          Text(bucketlistData[index]['cost'].toString() ?? ""),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
